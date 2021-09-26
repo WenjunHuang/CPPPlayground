1997db2c60295f52f7e646710d11429f86aa14e1
jatom <wenjun.huang80@gmail.com>
2021-09-26 15:02:46 +0800

    update

---
+++ qt5/dpi_device_pixel_ration.cc
@@ -0,0 +1,23 @@
+//
+// Created by rick on 2021/9/26.
+//
+#include <qpa/qplatformscreen.h>
+#include <QDebug>
+#include <QGuiApplication>
+#include <QScreen>
+
+int main(int argc, char* argv[]) {
+  QGuiApplication app(argc, argv);
+  auto screens = QGuiApplication::screens();
+  for (auto screen : screens) {
+    auto platform_screen = screen->handle();
+    auto default_dpi = platform_screen->logicalBaseDpi();
+    auto dpi = platform_screen->logicalDpi();
+    qDebug() << QString("screen %1").arg(screen->name());
+    qDebug() << QString("default dpi:{%1, %2}")
+                    .arg(default_dpi.first)
+                    .arg(default_dpi.second);
+    qDebug() << QString("current dpi:{%1, %2}").arg(dpi.first).arg(dpi.second);
+    qDebug() << "---------------";
+  }
+}
